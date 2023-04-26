56d8f006139a5e80d2ca1854aba8bab957aacbb6
maahnbraz <marinanbraz@gmail.com>
2023-04-26 16:03:39 -0300

    primeira versão

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,17 @@
-const cipher = {
-  // ...
-};
+//import encode from "./index";
+//import decode from "./index";
 
-export default cipher;
+function cesarCipher(message, shift) {
+  let result = "";
+  for (let i = 0; i < message.length; i++) {
+    let charCode = message.charCodeAt(i);
+    if (charCode >= 65 && charCode <= 90) {
+      result += String.fromCharCode(((charCode - 65 + shift) % 26) + 65);
+    } else if (charCode >= 97 && charCode <= 122) {
+      result += String.fromCharCode(((charCode - 97 + shift) % 26) + 97);
+    } else {
+      result += message.charAt(i);
+    }
+  }
+  return result;
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import cipher from './cipher.js';
+//import cesarCipher from "./cipher"
 
-console.log(cipher);
+function encode() {
+  const shift = parseInt(document.getElementById("shift").value);
+  const message = document.getElementById("message").value;
+  const result = document.getElementById("result");
+  result.value = cesarCipher(message, shift);
+}
+
+function decode() {
+  const shift = parseInt(document.getElementById("shift").value);
+  const message = document.getElementById("message").value;
+  const result = document.getElementById("result");
+  result.value = cesarCipher(message, -shift);
+}
